015b3c2b3e22dfd9368a7616faf44392c1ee4e36
Csxeba <csxeba@gmail.com>
2016-08-26 18:37:29 +0200

    Sequence works with keras LSTM!!!

--- keras_vs_petofi.py
+++ keras_vs_petofi.py
@@ -1,33 +1,45 @@
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
+from keras.optimizers import *
 
-from csxdata import Sequence, roots
+from csxdata import Sequence, roots, log
 from csxdata.utilities.helpers import speak_to_me
 
 
+
 def pull_data():
-    return Sequence(roots["txt"] + "petofi.txt", embeddim=10, cross_val=0.0, timestep=5)
+    return Sequence(roots["txt"] + "petofi.txt", embeddim=None, cross_val=0.0, timestep=10)
 
 
-def get_net():
+def get_net(inshape, outputs):
     model = Sequential([
-        LSTM(120, input_dim=(5, 10)),
-        Dense(10)
+        LSTM(180, input_shape=inshape, activation="tanh"),
+        Dense(60, activation="tanh"),
+        Dense(outputs, activation="softmax")
     ])
-    model.compile("adagrad", "mse")
+    model.compile(Adagrad(), "categorical_crossentropy")
     return model
 
 
 def xperiment():
+    log(" ----- ")
     petofi = pull_data()
-    model = get_net()
+    inshape, outputs = petofi.neurons_required
+    model = get_net(inshape, outputs)
 
     X, y = petofi.table("learning")
-    speak_to_me(model, petofi)
+    spoken = [speak_to_me(model, petofi)]
+    log(spoken[0])
+    print(spoken[0])
 
-    for century in range(1, 10):
+    for century in range(1, 20):
         model.fit(X, y, nb_epoch=100)
-        speak_to_me(model, petofi)
+        spoken.append("Epoch {}: {}".format(century * 100, speak_to_me(model, petofi)))
+        print(spoken[-1])
+        log(spoken[-1])
+
+    print("Run ended! Generated text:")
+    print("\n".join(spoken))
 
 if __name__ == '__main__':
     xperiment()
