fa8261d29a8d8cc5041efbcea8569b23efbb97a2
csa <csxeba@gmail.com>
2016-11-14 21:55:39 +0100

    goodnight

--- arbitrary_func.py
+++ arbitrary_func.py
@@ -1,18 +1,30 @@
 import numpy as np
+from keras.layers import Dense, Highway
 
-from csxnet import Network
+from keras.models import Sequential as Network
+
 from csxdata import RData
+from keras.optimizers import SGD
 
-
-X = np.random.uniform(-10.0, 10.0, size=(100000, 1))
+X = np.random.uniform(-360.0, 360.0, size=(100000, 1))
 y = np.sin(X)
 
 data = RData((X, y), cross_val=0.1, indeps_n=1, header=None)
 data.transformation = "std"
 
-net = Network(data, 0.01, 0.0, 0.0, 0.0, "mse", "TestDenseNet")
-net.add_fc(120)
-net.finalize_architecture()
+net = Network([
+    Dense(300, activation="tanh", input_dim=data.neurons_required[0]),
+    Dense(data.neurons_required[1], activation="tanh")
+])
+net.compile(SGD(lr=0.001, momentum=0.9), loss="mse")
 
-net.fit(20, 10)
+net.fit(X, y, batch_size=20, validation_split=0.1)
 
+test = np.arange(0, 361, 45, dtype=float)[:, None]
+preds = net.predict(test, verbose=0)
+preds = np.round(preds, 4)
+real = np.sin(test)
+real = np.round(real, 4)
+
+for tx, ptx, ty in zip(test.ravel(), preds.ravel(), real.ravel()):
+    print("sin({}) = {} (real: {})".format(tx, ptx, ty))
