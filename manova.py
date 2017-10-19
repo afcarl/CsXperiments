d1971e53dfa0accc5da688bafbaf1582a231e346
Csxeba <csxeba@gmail.com>
2017-10-19 05:56:19 +0200

    Added new datapoints

--- manova.py
+++ manova.py
@@ -3,23 +3,54 @@ import numpy as np
 from csxdata.utilities.vectorop import split_by_categories
 
 
-def _calc_scatters(X, Y):
+def _split_by_categories(X, Y):
     Xs = split_by_categories(Y, X)
-    N = np.array([len(x) for x in Xs.values()])
-    M = np.array([x.mean(axis=0) for x in Xs.values()])
-    mu = M.mean(axis=0)
-    Mc = np.array([m - mu for m in M])
-    Xc = np.concatenate([x - m for x, m in zip(Xs.values(), M)], axis=0)
-    Sw = (Xc.T @ Xc) / len(X)
-    Sb = (Mc.T @ Mc) * N / len(X)
+    Ns = np.array([len(x) for x in Xs.values()])
+    Ms = np.array([x.mean(axis=0) for x in Xs.values()])
+    return Xs, Ns, Ms
 
+
+
+def _calc_scatters(Xs, Ms, Ns):
+    N = sum(Ns)
+    mu = Ms.mean(axis=0)
+    Mc = np.array([m - mu for m in Ms])
+    Xc = np.concatenate([x - m for x, m in zip(Xs.values(), Ms)], axis=0)
+    Sw = (Xc.T @ Xc) / N
+    Sb = (Mc.T @ Mc) * Ns / N
     return Sw, Sb
 
 
+def _wilks_lambda_to_F(W, N, K, D):
+    a = N - K - ((D - K + 2) / 2)
+    b_denom = D**2 + (K - 1)**2 - 5
+    if b_denom > 0:
+        b_numer = (D**2 * (K - 1)**2 - 4)
+        b = np.sqrt(b_numer / b_denom)
+    else:
+        b = 1
+    c = 0.5 * 
+    wilks = np.prod(1. / 1. + W)
+
+
+def _calc_pillai_barlett_trace(W):
+    return np.sum(W / (1. + W))
+
+
+def _calc_hotelling_trace(W):
+    return np.sum(W)
+
+
+def _calc_roy_greatest_root(W):
+    return np.max(W)
+
+
 def manova(X, Y, p=0.05, stat="wilk"):
-    Sw, Sb = _calc_scatters(X, Y)
-    W = np.linalg.eigvals(np.linalg.inv(Sw) @ Sb)
-    Wilks = np.prod(1. / (1. + W))
+    Xs, Ns, Ms = _split_by_categories(X, Y)
+    Sw, Sb = _calc_scatters(Xs, Ns, Ms)
+    E = np.linalg.inv(Sw) @ Sb
+    W, V = np.linalg.eig(E)
+
 
 
 if __name__ == '__main__':
